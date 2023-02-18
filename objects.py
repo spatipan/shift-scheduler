8a6113205f83b6ae18d4fd9baef933885de7df36
Patipan Sitthiprawiat <patipan120897@gmail.com>
2023-02-18 15:49:10 +0700

    initialize timeinterval & timeintervals

--- objects.py
+++ objects.py
@@ -1,21 +1,60 @@
-# Description: This file contains the classes for the objects.
+# Availability class, define the availability of each person
+# list of datetime interval, each interval is a tuple of (start, end)
+# start and end are datetime object
+# Availability can be combined by using the + operator if intervals overlap, they will be combined into one interval
+# Availability can be subtracted by using the - operator, the result will be the availability of the first object minus the availability of the second object
 
-# Staff class contains the information about the staff members.
-class Staff():
-    def __init__(self, name, is_ai):
-        self.name = name
-        self.is_ai = is_ai
+class Availability():
+    """Availability class, define the availability of each person
+    list of datetime interval, each interval is a tuple of (start, end)
+    start and end are datetime object
+    Availability can be combined by using the + operator if intervals overlap, they will be combined into one interval
+    Availability can be subtracted by using the - operator, the result will be the availability of the first object minus the availability of the second object
+    """
 
-# Shift class contains the information about the shifts.
-class Shift():
-    def __init__(self, name):
-        self.name = name
 
-# ShiftRequest class contains the information about the shift requests.
-class ShiftRequest():
-    def __init__(self, staff, shift, day, priority):
-        self.staff = staff
-        self.shift = shift
-        self.day = day
-        self.priority = priority  # 1 is highest priority, 3 is lowest priority
+    def __init__(self, intervals):
+        self.intervals = intervals
+
+    def __add__(self, other):
+        intervals = self.intervals + other.intervals
+        intervals.sort(key=lambda x: x[0])
+        new_intervals = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= new_intervals[-1][1]:
+                new_intervals[-1] = (new_intervals[-1][0], max(intervals[i][1], new_intervals[-1][1]))
+            else:
+                new_intervals.append(intervals[i])
+        return Availability(new_intervals)
+
+    def __sub__(self, other):
+        intervals = self.intervals
+        for interval in other.intervals:
+            new_intervals = []
+            for i in range(len(intervals)):
+                if intervals[i][1] <= interval[0] or intervals[i][0] >= interval[1]:
+                    new_intervals.append(intervals[i])
+                elif intervals[i][0] < interval[0] and intervals[i][1] > interval[1]:
+                    new_intervals.append((intervals[i][0], interval[0]))
+                    new_intervals.append((interval[1], intervals[i][1]))
+                elif intervals[i][0] < interval[0] and intervals[i][1] <= interval[1]:
+                    new_intervals.append((intervals[i][0], interval[0]))
+                elif intervals[i][0] >= interval[0] and intervals[i][1] > interval[1]:
+                    new_intervals.append((interval[1], intervals[i][1]))
+            intervals = new_intervals
+        return Availability(intervals)
+
+    def get_hours(self):
+        hours = 0
+        for interval in self.intervals:
+            hours += (interval[1] - interval[0]).total_seconds() / 3600
+        return hours
+
+    # Plot the availability
+    def plot(self):
+        df = pd.DataFrame(columns=['Task', 'Start', 'Finish', 'Resource'])
+        for i in range(len(self.intervals)):
+            df.loc[i] = ['Task', self.intervals[i][0], self.intervals[i][1], 'Resource']
+        fig = ff.create_gantt(df, index_col='Resource', show_colorbar=True, group_tasks=True)
+        fig.show()
 
