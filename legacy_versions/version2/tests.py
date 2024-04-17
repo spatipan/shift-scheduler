ba457d482bd804023ee7c7a57bc7586d56d5e7af
Patipan Sitthiprawiat <patipan120897@gmail.com>
2024-04-17 09:58:55 +0700

    add gcf cloud function

--- legacy_versions/version2/tests.py
+++
@@ -1,4 +0,0 @@
-from tests.main_test import *
-
-if __name__ == "__main__":
-    test_objects.run_tests()
--- modules/google_app/__init__.py
+++
@@ -1,6 +0,0 @@
-from .calendar import CalendarApp
-from .google_app_auth import GoogleAppAuthenticator
-from .sheet import GoogleSheetApp
-
-__all__ = ['CalendarApp', 'GoogleAppAuthenticator', 'GoogleSheetApp']
-
