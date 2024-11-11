866bbc4e31b08d5e92545d810be9d4052fea787a
Patipan Sitthiprawiat <patipan120897@gmail.com>
2024-11-11 14:29:11 +0700

    fix authentication flow

--- modules/google_app/google_app_auth.py
+++ modules/google_app/google_app_auth.py
@@ -2,16 +2,11 @@ from datetime import datetime
 import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials 
-from google.auth.external_account_authorized_user import Credentials as ExternalAccountCredentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from google.auth.exceptions import RefreshError
 import logging
 import json
 
 from config import SECRET_PATH, CREDENTIALS, TOKEN
-
 
 class GoogleAppAuthenticator:
     def __init__(self, SCOPES: list = ['https://www.googleapis.com/auth/calendar.readonly', 
@@ -47,16 +42,15 @@ class GoogleAppAuthenticator:
                         creds.refresh(Request())
                         self.logger.debug('Credentials refreshed successfully')
                     except RefreshError:
-                        st.warning("Token expired or revoked. Please re-authenticate.")
-                        self._handle_reauthentication_flow()
+                        self.logger.error("Token expired or revoked. Re-authentication required.")
+                        raise ValueError("Token expired or revoked.")
                 else:
-                    st.warning("No valid token found. Re-authentication is required.")
-                    self._handle_reauthentication_flow()
+                    self.logger.error("No valid token found, re-authentication required.")
+                    raise ValueError("Re-authentication required due to invalid or expired credentials.")
 
         except RefreshError as e:
             self.logger.error("RefreshError encountered: Token has expired or been revoked.")
-            st.error("Token has been expired or revoked. Please re-authenticate.")
-            self._handle_reauthentication_flow()
+            raise ValueError("Token has been expired or revoked. Please re-authenticate.")
 
         except Exception as e:
             self.logger.error(f'Error during authentication: {e}')
@@ -67,26 +61,6 @@ class GoogleAppAuthenticator:
         self.authenticated = True
         return self.credentials
 
-    def _handle_reauthentication_flow(self):
-        """Handle re-authentication flow for Streamlit."""
-        self.logger.debug('Deleting token...')
-        self._delete_token()
-        self.logger.debug('Starting re-authentication flow...')
-        flow = InstalledAppFlow.from_client_config(self.credentials, self.SCOPES)
-        auth_url, _ = flow.authorization_url(prompt='consent')
-        st.markdown(f"[Click here to authorize]({auth_url})")
-        auth_code = st.text_input("Paste the authorization code here:")
-
-        if auth_code:
-            try:
-                creds = flow.fetch_token(code=auth_code)
-                st.success("Authentication successful!")
-                self._save_credentials(creds)
-                self.credentials = creds
-            except Exception as e:
-                st.error(f"Failed to fetch token: {e}")
-                self.logger.error(f"Error fetching token: {e}")
-    
     def _save_credentials(self, creds):
         '''Save credentials to a file'''
         with open(SECRET_PATH, "r") as file:
@@ -97,7 +71,7 @@ class GoogleAppAuthenticator:
             json.dump(secret, file)
             self.logger.debug('Credentials saved to file')
 
-    ##TODO: Function to delete token from secret file   
+    ## Function to delete token from secret file   
     def _delete_token(self):
         '''Delete token from secret file'''
         with open(SECRET_PATH, "r") as file:
@@ -107,8 +81,6 @@ class GoogleAppAuthenticator:
         with open(SECRET_PATH, "w") as file:
             json.dump(secret, file)
             self.logger.debug('Token deleted from file')
-      
-
 
 if __name__ == '__main__':
     authenticator = GoogleAppAuthenticator()
@@ -116,4 +88,3 @@ if __name__ == '__main__':
         credentials = CREDENTIALS,
         token = TOKEN,
     )
-
